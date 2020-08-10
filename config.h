78675f19773fa04822c1fd9f60855d84d3375591
Michal Kukla <michal.kukla@tul.cz>
2020-08-10 10:54:33 +0200

    Added battery and internet connection

--- config.h
+++ config.h
@@ -1,6 +1,8 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"", "connet",						 5,		6},
+	{"", "battery",						 5, 		5},
 	{"", "partitions",					 5,		4},
 	{"", "volume",						 0, 		3},
 	{"", "keyboard-layout", 				 0, 	        2},
