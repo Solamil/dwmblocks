43a66575c7b6e35f08e5a2997a7e5fecae5729f8
Michal Kukla <michal.kukla@tul.cz>
2020-11-01 20:49:49 +0100

    Uncomment clock

--- config.h
+++ config.h
@@ -6,10 +6,10 @@ static const Block blocks[] = {
 	{"", "partitions",					 5,		4},
 	{"", "volume",						 0, 		3},
 	{"", "keyboard-layout", 				 0, 	        2},
-//	{"", "clock",						50,		1},
+	{"", "clock",						50,		1},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
 //static char delim = '|';
 static char *delim = " | ";
-static unsigned int delimLen = 5;
+static unsigned int delimLen = 6;
